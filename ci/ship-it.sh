c8ba0113834d2acd20b37d917242fa979140f6ef
Oscar David Arbeláez <odarbelaeze@gmail.com>
2018-07-25 07:29:57 -0500

    Too sleepy

--- ci/ship-it.sh
+++ ci/ship-it.sh
@@ -1,4 +1,4 @@
 wget https://github.com/github/hub/releases/download/v2.5.0/hub-linux-amd64-2.5.0.tgz
-tar xvzf hub-linux-amd64-2.5.0
+tar xvzf hub-linux-amd64-2.5.0.tgz
 export PATH=$PWD/hub-linux-amd64-2.5.0:$PATH
 hub version
