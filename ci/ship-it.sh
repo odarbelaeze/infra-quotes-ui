067f275b3380c0ab6562e1dcf3fc5a12c45292ff
Oscar David Arbeláez <odarbelaeze@gmail.com>
2018-07-25 23:01:07 -0500

    Weird environment variable

--- ci/ship-it.sh
+++ ci/ship-it.sh
@@ -1,4 +1,5 @@
 wget https://github.com/github/hub/releases/download/v2.5.0/hub-linux-amd64-2.5.0.tgz
 tar xvzf hub-linux-amd64-2.5.0.tgz
-export PATH=$PWD/hub-linux-amd64-2.5.0:$PATH
+export PATH="$(pwd)/hub-linux-amd64-2.5.0:$PATH"
+echo $PATH
 hub version
